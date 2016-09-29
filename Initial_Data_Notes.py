a16d9b1d0b4efcab8766bdcd4b70c2b00fcd77e8
gloriadazevedo <gloria.dazevedo@gmail.com>
2016-09-29 15:50:05 -0400

    Added Comments

--- Initial_Data_Notes.py
+++ Initial_Data_Notes.py
@@ -3,3 +3,5 @@
 import csv
 
 filename=open('C:\\Users\\glori\\Documents\\GitHub\\ORIE4741_Project\\Speed Dating Data.csv','r')
+
+#Want to look at ranges of predictors and figure out which ones are important
