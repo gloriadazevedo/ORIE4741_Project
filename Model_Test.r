f0848dd7c6080d9fa139738cc668d33c5ed0c5a2
gloriadazevedo <gloria.dazevedo@gmail.com>
2016-10-19 21:05:02 -0400

    Added loop to scrape data for relevant columns

--- Model_Test.r
+++ Model_Test.r
@@ -16,12 +16,13 @@
 #match data to test it on the other disjoint half or use bootstrap or k-fold cross validation,
 #then we will divide the total matrix up.
 
-column_vector<-c(race,field_cd)
+column_vector<-c("race","field_cd")
 
 #Use an external counter for the subset data matrix and result vector to explicitly
 #assign data to the corresponding rows instead of "append" 
+#Thus we have to have twice the number of columns in the matrix than in the vector of column names that we want
 result_vector<-rep(0,total_interactions)
-return_matrix<-matrix(rep(0,length(column_vector)*total_interactions),ncol=length(column_vector),nrow=total_interactions)
+return_matrix<-matrix(rep(0,2*length(column_vector)*total_interactions),ncol=2*length(column_vector),nrow=total_interactions)
 counter<-1
 
 #Need to iterate through each wave to get the id's of males and females
@@ -33,11 +34,23 @@ scrape_data<-function(column_vector){
 		
 		#Data extraction by match
 		for (i in 1:length(female_id)){
+			#Need to just extract the relevant columns and rows for the female
+			f_vector<-full_data[full_data$wave==w & full_data$gender==0 & 
+			full_data$id==female_id[i],column_vector][1,]
+			
 			for (j in 1:length(male_id)){
+				#Need to extract the relevant columns and rows for the male
+				m_vector<-full_data[full_data$wave==w & full_data$gender==1 & 
+					full_data$id==male_id[j],column_vector][1,]
 				
+				#Obtain the match value for the vector
+				match_value<-full_data[full_data$wave==w & full_data$gender==0 & 
+				full_data$id==female_id[i] & full_data$partner==male_id[j], ]$"match"
 				
 				#Update the resulting classification vector
 				result_vector[counter]<-match_value
+				return_matrix[counter,1:length(column_vector)]<-t(f_vector)
+				return_matrix[counter,(length(column_vector)+1):(2*length(column_vector))]<-t(m_vector)
 				counter<-counter+1
 			}
 		}
